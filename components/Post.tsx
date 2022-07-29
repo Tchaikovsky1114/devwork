195e7dcb228494ef20e9435af25996899b308738
Kim Myungseong <forwarm5891@gmail.com>
2022-07-29 10:02:34 +0900

    feat: create post header and image section

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {DotsHorizontalIcon} from '@heroicons/react/solid'
 interface PostProps{
   id:number
   username:string
@@ -10,8 +10,16 @@ interface PostProps{
 
 const Post = ({id,username,userImage,caption,image}:PostProps) => {
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className='bg-white my-8 border rounded-md'>
+      {/* Header */}
+      <div className="flex items-center p-5">
+        <img className="w-12 h-12 rounded-full object-cover p-[1px] border mr-3" src={userImage} alt={username} />
+        <p className="font-bold flex-1">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+
+      {/* Post Image */}
+      <img className="object-cover w-full" src={image} alt="post-image" />
     </div>
   );
 };
